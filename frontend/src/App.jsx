97999ddc021ba633b3b7fd5bb3b9cbea9b10215c
The-morning-star23 <kumarshubh263@gmail.com>
2025-06-11 21:42:28 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing';
-import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard'
