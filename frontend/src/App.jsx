7dbd7805ade5919ef109a768e6d82d0a4352cdf5
The-morning-star23 <kumarshubh263@gmail.com>
2025-06-12 20:28:40 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Landing from './pages/Landing';
 import Login from './pages/Login'
 import Register from './pages/Register';
 import Header from './pages/Header';
-
+import CareerMatch from './pages/CareerMatch';
 function App() {
   return (
     <Router>
